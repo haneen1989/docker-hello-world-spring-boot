83ef1aaa2026876a439481f8d8ab7c9b5deba8ce
EC2 Default User <ec2-user@ip-10-0-0-122.eu-west-3.compute.internal>
2023-03-08 18:41:09 +0000

    initial

---
+++ Dockerscript.sh
@@ -0,0 +1,19 @@
+FILE=Dockerfile
+IMAGE_NAME=imagename
+REGISTRY=vidyasg
+REPOSITORY=docker
+TAG_VERSION=$2
+TOKEN=$1
+
+if test -f "$File"; then
+  echo "$File" exists
+  echo "*************Docker image build started************"
+  docker build -t $IMAGE_NAME .
+  echo "*************Docker image build completed***********"
+  echo "*************Push docker image to docker hub***********"
+  docker tag $IMAGE_NAME $REGISTRY/$REPOSITORY:$TAG_VERSION
+  docker login -u $REGISTRY -p $TOKEN
+  docker push $REGISTRY/$REPOSITORY:$TAG_VERSION
+else
+  echo "**********Dockerfile doesnot exists*************"
+fi
